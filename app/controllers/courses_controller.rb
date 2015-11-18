19a34604039198003fbfa65eb7d8823bee104c82
MuhammadTamzid <muhammad.tamzid@gmail.com>
2015-11-18 15:38:47 +0600

    enrollment add edit supervisors trainees

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,14 +1,8 @@
 class CoursesController < ApplicationController
-  before_action :load_course, only: :show
-
   def index
+    @courses = current_user.courses
   end
 
   def show
-  end
-
-  private
-  def load_course
-    @course = Course.find params[:id]
   end
 end
--- app/models/course_user.rb
+++ app/models/course_user.rb
@@ -1,4 +1,23 @@
 class CourseUser < ActiveRecord::Base
   belongs_to :course
   belongs_to :user
+
+  validates :course_id, uniqueness: {scope: :user_id}
+
+  scope :has_user, -> user {find_by user_id: user.id}
+
+  after_save :enroll_user_to_subjects
+  after_destroy :delete_user_subjects
+
+  private
+  def enroll_user_to_subjects
+    course.course_subjects.each do |course_subject|
+      user.user_subjects.create subject_id: course_subject.subject_id,
+        course_id: course_id
+    end
+  end
+
+  def delete_user_subjects
+    course.user_subjects.search_by_user(user_id).delete_all
+  end
 end
